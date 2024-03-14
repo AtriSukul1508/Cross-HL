efe476cb52af02eab82e98a3758a75b4fb67ce8a
Atri Sukul <95478617+AtriSukul1508@users.noreply.github.com>
2024-03-14 13:02:59 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,34 +1,3 @@
-import sys
-sys.path.append("./../")
-import os
-import numpy as np
-import random
-from torch import einsum
-import torch.nn.functional as F
-import torch
-import torch.nn as nn
-import torch.utils.data as dataf
-from torch.utils.data import Dataset
-from scipy import io
-from scipy.io import loadmat as loadmat
-from sklearn.decomposition import PCA
-from torch.nn.parameter import Parameter
-import torchvision.transforms.functional as TF
-import time
-from PIL import Image
-import math
-from operator import truediv
-from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, cohen_kappa_score
-from torchsummary import summary
-import matplotlib.pyplot as plt
-import logger
-import torch.backends.cudnn as cudnn
-import re
-from pathlib import Path
-import copy
-cudnn.deterministic = True
-cudnn.benchmark = False
-
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 datasetNames = ["Trento"] # ["Trento", "MUUFL", "Houston"]
 MultiModalData = 'LiDAR'
@@ -37,7 +6,7 @@ modelName = 'Cross-HL'
 patchsize = 11
 batch_size = 64 # batch size for training 
 test_batch_size = 500
-EPOCHS = 100
+EPOCHS = 10
 learning_rate = 5e-4
 FM = 16
 FileName = 'CrossHL'
@@ -63,30 +32,30 @@ for dataset in datasetNames:
 
         train_dataset = HSI_LiDAR_DatasetTrain(dataset=dataset)
         test_dataset = HSI_LiDAR_DatasetTest(dataset=dataset)
-        print(len(train_dataset),len(test_dataset))
-        NC = train_dataset.hs_ims.shape[1]
+        # print(len(train_dataset),len(test_dataset))
+        NC = train_dataset.hs_image.shape[1]
 
-        NCLidar = train_dataset.lid_ims.shape[1]
-        Classes = len(torch.unique(train_dataset.lbs))
+        NCLidar = train_dataset.lidar_image.shape[1]
+        Classes = len(torch.unique(train_dataset.lbls))
 
         train_loader = dataf.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers= 4)
 
 
-        test_patch_hsi = test_dataset.hs_ims
-        test_patch_lidar = test_dataset.lid_ims
-        test_label = test_dataset.lbs
+        test_patch_hsi = test_dataset.hs_image
+        test_patch_lidar = test_dataset.lidar_image
+        test_label = test_dataset.lbls
 
         KAPPA = []
         OA = []
         AA = []
-        ELEMENT_ACC = np.zeros((num_iterations, Classes)) # 3xNC
+        ELEMENT_ACC = np.zeros((num_iterations, Classes)) # num_iterationsxNC
 
         seed_val(14)
         for iterNum in range(num_iterations):
             print('\n')
             print("---------------------------------- Summary ---------------------------------------------")
             print('\n')
-            model = FuseFormer(FM=FM, NC=NC, NCLidar=NCLidar, Classes=Classes,patchsize = patchsize).cuda()
+            model = CrossHL_Transformer(FM=FM, NC=NC, NCLidar=NCLidar, Classes=Classes,patchsize = patchsize).cuda()
             summary(model, [(NC, patchsize**2),(NCLidar,patchsize**2)])
 
             optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,weight_decay=5e-3)
@@ -160,7 +129,7 @@ for dataset in datasetNames:
 
             model.eval()
 
-            confusion_mat, overall_acc, class_acc, avg_acc, kappa_score = result_reports(test_patch_hsi,test_patch_lidar,test_label,dataset,model, iterNum)
+            confusion_mat, overall_acc, class_acc, avg_acc, kappa_score = utils.result_reports(test_patch_hsi,test_patch_lidar,test_label,dataset,model, iterNum)
             KAPPA.append(kappa_score)
             OA.append(overall_acc)
             AA.append(avg_acc)
